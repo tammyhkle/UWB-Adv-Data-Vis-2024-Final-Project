cbc765d3d0223dd46cc70869a0118b5ad70cd213
rachelfgraham <81714517+rachelfgraham@users.noreply.github.com>
2024-02-19 20:12:05 -0800

    Created shiny app and displayed long format dataset as a data table

---
+++ SMRT_shinyapp/server.R
@@ -0,0 +1,38 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(DT)
+
+function(input, output, session) {
+
+  data <- reactive({
+    # Importing Data
+    library(readxl)
+    Seattle_Car_Crashes <- read_excel("Seattle Car Crashes.xlsx")
+    
+    # Wrangling the Data Set into long Format
+    library(tidyverse)
+    Seattle_Car_Crashes$INJURIES <- as.character(Seattle_Car_Crashes$INJURIES)
+    Seattle_Car_Crashes$SERIOUSINJURIES <- as.character(Seattle_Car_Crashes$SERIOUSINJURIES)
+    Seattle_Car_Crashes$FATALITIES <- as.character(Seattle_Car_Crashes$FATALITIES)
+    Seattle_Car_Crashes$INCDATE <- as.character.Date(Seattle_Car_Crashes$INCDATE)
+    Seattle_Car_Crashes$INCDTTM <- as.character(Seattle_Car_Crashes$INCDTTM)
+    Seattle_Car_Crashes <- subset(Seattle_Car_Crashes, select = -c(INCDTTM))
+    Seattle_Car_Crashes_long <- Seattle_Car_Crashes %>% 
+      pivot_longer(cols = everything(), values_drop_na = TRUE)
+    
+    Seattle_Car_Crashes_long
+  })
+  
+  # Render the data as a DataTable
+  output$mytable <- renderDT({
+    datatable(data())
+  })
+}
---
+++ SMRT_shinyapp/ui.R
@@ -0,0 +1,18 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(DT)
+
+fluidPage(
+  titlePanel("Seattle Car Crashes"),
+  mainPanel(
+    DTOutput("mytable")
+  )
+)
